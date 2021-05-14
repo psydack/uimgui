b44bc08b1f4ba753b7f01ff6542bca186b21303d
Rofli Sanches <rofli.souza@insanemedia.com.br>
2021-05-14 00:54:02 -0300

    refactor: mover uiconfig to data folder

--- Source/Data/UIOConfig.cs
+++ Source/Data/UIOConfig.cs
@@ -8,7 +8,7 @@ namespace UImGui
 	/// TODO: Write more.
 	/// </summary>
 	[System.Serializable]
-	internal struct IOConfig
+	internal struct UIOConfig
 	{
 		// TODO: Test all flags.
 		[Tooltip("For more info look the imgui.h:1380(~). (default=NavEnableKeyboard | DockingEnable)")]
