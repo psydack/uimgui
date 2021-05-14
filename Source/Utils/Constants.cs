834e42bf6849821cea28f6e10032b7410849a022
Rofli Sanches <rofli.souza@insanemedia.com.br>
2021-05-14 00:57:53 -0300

    feat: add constants with marker and version vars

---
+++ Source/Utils/Constants.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Profiling;
+
+namespace UImGui
+{
+	public static class Constants
+	{
+		public static readonly ProfilerMarker PrepareFrameMarker = new ProfilerMarker("DearImGui.PrepareFrame");
+		public static readonly ProfilerMarker LayoutfMarker = new ProfilerMarker("DearImGui.Layout");
+		public static readonly ProfilerMarker DrawListMarker = new ProfilerMarker("DearImGui.RenderDrawLists");
+
+		public static readonly uint Version = (0 << 16) | (0 << 8) | (5);
+	}
+}
