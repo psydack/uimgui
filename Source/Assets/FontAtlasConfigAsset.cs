cfdb5ac95c520d28344527e02b6277630ee1d7a5
Rofli Sanches <rofli.souza@insanemedia.com.br>
2021-05-14 01:03:32 -0300

    feat: add font configuration as scriptable object

---
+++ Source/Assets/FontAtlasConfigAsset.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UImGui.Assets
+{
+	// TODO: Make a default resource file as sample.
+	[CreateAssetMenu(menuName = "Dear ImGui/Font Atlas Configuration")]
+	internal sealed class FontAtlasConfigAsset : ScriptableObject
+	{
+		public FontRasterizerType Rasterizer;
+		public uint RasterizerFlags;
+		public FontDefinition[] Fonts;
+	}
+}
