411eb63141d68979e6131d47ad624d7c4d502a0d
Rofli Sanches <rofli.souza@insanemedia.com.br>
2021-05-14 01:04:17 -0300

    feat: add shader resources as scriptable object

---
+++ Source/Assets/ShaderResourcesAsset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace UImGui.Assets
+{
+	[CreateAssetMenu(menuName = "Dear ImGui/Shader Resources")]
+	internal sealed class ShaderResourcesAsset : ScriptableObject
+	{
+		public ShaderData Shader;
+		public ShaderProperties PropertyNames;
+	}
+}
