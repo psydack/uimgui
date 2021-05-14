db9df9bccf7b1eedde82633669834d09181c4cf2
Rofli Sanches <rofli.souza@insanemedia.com.br>
2021-05-14 01:08:26 -0300

    feat: add shader data and properties

---
+++ Source/Data/Shader/ShaderData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UImGui
+{
+	[Serializable]
+	internal class ShaderData
+	{
+		public ShaderData Mesh;
+		public ShaderData Procedural;
+
+		public ShaderData Clone()
+		{
+			return (ShaderData)MemberwiseClone();
+		}
+	}
+}
---
+++ Source/Data/Shader/ShaderProperties.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UImGui
+{
+	[Serializable]
+	internal class ShaderProperties
+	{
+		public string Texture;
+		public string Vertices;
+		public string BaseVertex;
+
+		public ShaderProperties Clone()
+		{
+			return (ShaderProperties)MemberwiseClone();
+		}
+	}
+}
