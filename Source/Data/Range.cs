929984395ed369762d1cc5549dd1e85d7a0f89b5
Rofli Sanches <rofli.souza@insanemedia.com.br>
2021-05-14 01:05:21 -0300

    feat: add range and spriteInfo class

---
+++ Source/Data/Range.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace UImGui
+{
+	[Serializable]
+	internal struct Range
+	{
+		public ushort Start;
+		public ushort End;
+	}
+}
---
+++ Source/Data/SpriteInfo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UTexture = UnityEngine.Texture;
+
+namespace UImGui.Texture
+{
+	internal sealed class SpriteInfo
+	{
+		public UTexture Texture;
+		public Vector2 Size;
+		public Vector2 UV0;
+		public Vector2 UV1;
+	}
+}
