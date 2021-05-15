55e413a59b5f7de5241b288fbc49b2bef462fe15
Rofli Sanches <rofli.souza@insanemedia.com.br>
2021-05-15 01:47:44 -0300

    feat: add utils class with commom methods

---
+++ Source/Utils/Utils.cs
@@ -0,0 +1,55 @@
+﻿using ImGuiNET;
+using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+using UnityEngine;
+
+namespace UImGui
+{
+	internal static unsafe class Utils
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal static System.Numerics.Vector2 ScreenToImGui(in Vector2 point)
+		{
+			return new System.Numerics.Vector2(point.x, ImGui.GetIO().DisplaySize.Y - point.y);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal static Vector2 ImGuiToScreen(in System.Numerics.Vector2 point)
+		{
+			return new Vector2(point.X, ImGui.GetIO().DisplaySize.Y - point.Y);
+		}
+
+		internal static string StringFromPtr(byte* ptr)
+		{
+			int characters = 0;
+			while (ptr[characters] != 0)
+			{
+				characters++;
+			}
+
+			return Encoding.UTF8.GetString(ptr, characters);
+		}
+
+		internal static int GetUtf8(string text, byte* utf8Bytes, int utf8ByteCount)
+		{
+			fixed (char* utf16Ptr = text)
+			{
+				return Encoding.UTF8.GetBytes(utf16Ptr, text.Length, utf8Bytes, utf8ByteCount);
+			}
+		}
+
+		internal static int GetUtf8(string text, int start, int length, byte* utf8Bytes, int utf8ByteCount)
+		{
+			if (start < 0 || length < 0 || start + length > text.Length)
+			{
+				throw new ArgumentOutOfRangeException();
+			}
+
+			fixed (char* utf16Ptr = text)
+			{
+				return Encoding.UTF8.GetBytes(utf16Ptr + start, length, utf8Bytes, utf8ByteCount);
+			}
+		}
+	}
+}
