52bc497d8dfe5b11fe6a45342d9caa52b319b315
Rofli Sanches <rofli.souza@insanemedia.com.br>
2021-05-15 01:46:58 -0300

    feat: add platform utility

---
+++ Source/Platform/PlatformUtility.cs
@@ -0,0 +1,25 @@
+﻿using UImGui.Assets;
+using UnityEngine;
+
+namespace UImGui.Platform
+{
+	internal static class PlatformUtility
+	{
+		internal static IPlatform Create(InputType type, CursorShapesAsset cursors, IniSettingsAsset iniSettings)
+		{
+			switch (type)
+			{
+				case InputType.InputManager:
+					return new InputManagerPlatform(cursors, iniSettings);
+				// TODO: Implement InputSystemPlatform.
+#if HAS_INPUTSYSTEM
+				//case InputType.InputSystem:
+				//	return new InputSystemPlatform(cursors, iniSettings);
+#endif
+				default:
+					Debug.LogError($"[DearImGui] {type} platform not available.");
+					return null;
+			}
+		}
+	}
+}
