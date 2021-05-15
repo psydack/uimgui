88a91ab03b13e024e794923289e07c2eb07e18aa
Rofli Sanches <rofli.souza@insanemedia.com.br>
2021-05-15 14:25:10 -0300

    refactor: change to internal and reorder properties

--- Source/Utils/Constants.cs
+++ Source/Utils/Constants.cs
@@ -2,14 +2,20 @@
 
 namespace UImGui
 {
-	public static class Constants
+	internal static class Constants
 	{
-		public static readonly string UImGuiCommandBuffer = "UImGui";
-
-		public static readonly ProfilerMarker PrepareFrameMarker = new ProfilerMarker("DearImGui.PrepareFrame");
-		public static readonly ProfilerMarker LayoutfMarker = new ProfilerMarker("DearImGui.Layout");
-		public static readonly ProfilerMarker DrawListMarker = new ProfilerMarker("DearImGui.RenderDrawLists");
-
 		public static readonly uint Version = (0 << 16) | (0 << 8) | (5);
+
+		internal static readonly string UImGuiCommandBuffer = "UImGui";
+
+		// TODO: Test all profile markers.
+		internal static readonly ProfilerMarker PrepareFrameMarker = new ProfilerMarker("DearImGui.PrepareFrame");
+		internal static readonly ProfilerMarker LayoutfMarker = new ProfilerMarker("DearImGui.Layout");
+		internal static readonly ProfilerMarker DrawListMarker = new ProfilerMarker("DearImGui.RenderDrawLists");
+
+		internal static readonly ProfilerMarker UpdateMeshPerfMarker = new ProfilerMarker("DearImGui.RendererMesh.UpdateMesh");
+		internal static readonly ProfilerMarker CreateDrawComandsPerfMarker = new ProfilerMarker("DearImGui.RendererMesh.CreateDrawCommands");
+
+		internal static readonly string ExecuteDrawComandsPerfMarker = "DearImGui.ExecuteDrawCommands";
 	}
 }
