e91173dc4c87a26d5f794bda5a70dbd56af5cb43
Rofli Sanches <kokerx@gmail.com>
2024-02-23 17:54:43 -0300

    add input for simple legacy system

--- Source/Platform/InputManagerPlatform.cs
+++ Source/Platform/InputManagerPlatform.cs
@@ -1,4 +1,4 @@
-using ImGuiNET;
+﻿using ImGuiNET;
 using UImGui.Assets;
 using UnityEngine;
 
@@ -30,8 +30,6 @@ namespace UImGui.Platform
 		{
 			base.Initialize(io, config, platformName);
 
-			SetupKeyboard(io);
-
 			return true;
 		}
 
@@ -44,56 +42,16 @@ namespace UImGui.Platform
 			UpdateCursor(io, ImGui.GetMouseCursor());
 		}
 
-		private void SetupKeyboard(ImGuiIOPtr io)
-		{
-			/*
-			// Map and store new keys by assigning io.KeyMap and setting value of array
-			_mainKeys = new int[] {
-				io.KeyMap[(int)ImGuiKey.A] = (int)KeyCode.A, // For text edit CTRL+A: select all.
-				io.KeyMap[(int)ImGuiKey.C] = (int)KeyCode.C, // For text edit CTRL+C: copy.
-				io.KeyMap[(int)ImGuiKey.V] = (int)KeyCode.V, // For text edit CTRL+V: paste.
-				io.KeyMap[(int)ImGuiKey.X] = (int)KeyCode.X, // For text edit CTRL+X: cut.
-				io.KeyMap[(int)ImGuiKey.Y] = (int)KeyCode.Y, // For text edit CTRL+Y: redo.
-				io.KeyMap[(int)ImGuiKey.Z] = (int)KeyCode.Z, // For text edit CTRL+Z: undo.
-
-				io.KeyMap[(int)ImGuiKey.Tab] = (int)KeyCode.Tab,
-
-				io.KeyMap[(int)ImGuiKey.LeftArrow] = (int)KeyCode.LeftArrow,
-				io.KeyMap[(int)ImGuiKey.RightArrow] = (int)KeyCode.RightArrow,
-				io.KeyMap[(int)ImGuiKey.UpArrow] = (int)KeyCode.UpArrow,
-				io.KeyMap[(int)ImGuiKey.DownArrow] = (int)KeyCode.DownArrow,
-
-				io.KeyMap[(int)ImGuiKey.PageUp] = (int)KeyCode.PageUp,
-				io.KeyMap[(int)ImGuiKey.PageDown] = (int)KeyCode.PageDown,
-
-				io.KeyMap[(int)ImGuiKey.Home] = (int)KeyCode.Home,
-				io.KeyMap[(int)ImGuiKey.End] = (int)KeyCode.End,
-				io.KeyMap[(int)ImGuiKey.Insert] = (int)KeyCode.Insert,
-				io.KeyMap[(int)ImGuiKey.Delete] = (int)KeyCode.Delete,
-				io.KeyMap[(int)ImGuiKey.Backspace] = (int)KeyCode.Backspace,
-
-				io.KeyMap[(int)ImGuiKey.Space] = (int)KeyCode.Space,
-				io.KeyMap[(int)ImGuiKey.Escape] = (int)KeyCode.Escape,
-				io.KeyMap[(int)ImGuiKey.Enter] = (int)KeyCode.Return,
-				io.KeyMap[(int)ImGuiKey.KeyPadEnter] = (int)KeyCode.KeypadEnter,
-			};*/
-		}
-
 		private void UpdateKeyboard(ImGuiIOPtr io)
 		{
-			/*
-			for (int keyIndex = 0; keyIndex < _mainKeys.Length; keyIndex++)
+			// BUG: mod key make everything slow. Go to line
+			foreach (KeyCode keyCode in System.Enum.GetValues(typeof(KeyCode)))
 			{
-				int key = _mainKeys[keyIndex];
-				io.KeysDown[key] = Input.GetKey((KeyCode)key);
+				if (TryMapKeys(keyCode, out ImGuiKey imguikey))
+				{
+					io.AddKeyEvent(imguikey, Input.GetKey(keyCode));
+				}
 			}
-
-			// Keyboard modifiers.
-			io.KeyShift = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-			io.KeyCtrl = Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
-			io.KeyAlt = Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt);
-			io.KeySuper = Input.GetKey(KeyCode.LeftCommand) || Input.GetKey(KeyCode.RightCommand) ||
-				Input.GetKey(KeyCode.LeftWindows) || Input.GetKey(KeyCode.RightWindows);
 
 			// Text input.
 			while (Event.PopEvent(_textInputEvent))
@@ -104,7 +62,6 @@ namespace UImGui.Platform
 					io.AddInputCharacter(_textInputEvent.character);
 				}
 			}
-			*/
 		}
 
 		private static void UpdateMouse(ImGuiIOPtr io)
@@ -115,6 +72,70 @@ namespace UImGui.Platform
 			io.AddMouseButtonEvent(1, Input.GetMouseButton(1));
 			io.AddMouseButtonEvent(2, Input.GetMouseButton(2));
 			io.AddMouseWheelEvent(Input.mouseScrollDelta.x, Input.mouseScrollDelta.y);
+		}
+
+		private static bool TryMapKeys(KeyCode key, out ImGuiKey imguikey)
+		{
+			static ImGuiKey KeyToImGuiKeyShortcut(KeyCode keyToConvert, KeyCode startKey1, ImGuiKey startKey2)
+			{
+				int changeFromStart1 = (int)keyToConvert - (int)startKey1;
+				return startKey2 + changeFromStart1;
+			}
+
+			imguikey = key switch
+			{
+				>= KeyCode.F1 and <= KeyCode.F12 => KeyToImGuiKeyShortcut(key, KeyCode.F1, ImGuiKey.F1),
+				>= KeyCode.Keypad0 and <= KeyCode.Keypad9 => KeyToImGuiKeyShortcut(key, KeyCode.Keypad0, ImGuiKey.Keypad0),
+				>= KeyCode.A and <= KeyCode.Z => KeyToImGuiKeyShortcut(key, KeyCode.A, ImGuiKey.A),
+				>= KeyCode.Alpha0 and <= KeyCode.Alpha9 => KeyToImGuiKeyShortcut(key, KeyCode.Alpha0, ImGuiKey._0),
+				// BUG: mod keys make everything slow. 
+				// KeyCode.LeftShift or KeyCode.RightShift => ImGuiKey.ModShift,
+				// KeyCode.LeftControl or KeyCode.RightControl => ImGuiKey.ModCtrl,
+				// KeyCode.LeftAlt or KeyCode.RightAlt => ImGuiKey.ModAlt,
+				// KeyCode.LeftWindows or KeyCode.RightWindows => ImGuiKey.ModSuper,
+				KeyCode.Menu => ImGuiKey.Menu,
+				KeyCode.UpArrow => ImGuiKey.UpArrow,
+				KeyCode.DownArrow => ImGuiKey.DownArrow,
+				KeyCode.LeftArrow => ImGuiKey.LeftArrow,
+				KeyCode.RightArrow => ImGuiKey.RightArrow,
+				KeyCode.Return => ImGuiKey.Enter,
+				KeyCode.Escape => ImGuiKey.Escape,
+				KeyCode.Space => ImGuiKey.Space,
+				KeyCode.Tab => ImGuiKey.Tab,
+				KeyCode.Backspace => ImGuiKey.Backspace,
+				KeyCode.Insert => ImGuiKey.Insert,
+				KeyCode.Delete => ImGuiKey.Delete,
+				KeyCode.PageUp => ImGuiKey.PageUp,
+				KeyCode.PageDown => ImGuiKey.PageDown,
+				KeyCode.Home => ImGuiKey.Home,
+				KeyCode.End => ImGuiKey.End,
+				KeyCode.CapsLock => ImGuiKey.CapsLock,
+				KeyCode.ScrollLock => ImGuiKey.ScrollLock,
+				KeyCode.Print => ImGuiKey.PrintScreen,
+				KeyCode.Pause => ImGuiKey.Pause,
+				KeyCode.Numlock => ImGuiKey.NumLock,
+				KeyCode.KeypadDivide => ImGuiKey.KeypadDivide,
+				KeyCode.KeypadMultiply => ImGuiKey.KeypadMultiply,
+				KeyCode.KeypadMinus => ImGuiKey.KeypadSubtract,
+				KeyCode.KeypadPlus => ImGuiKey.KeypadAdd,
+				KeyCode.KeypadPeriod => ImGuiKey.KeypadDecimal,
+				KeyCode.KeypadEnter => ImGuiKey.KeypadEnter,
+				KeyCode.KeypadEquals => ImGuiKey.KeypadEqual,
+				KeyCode.Tilde => ImGuiKey.GraveAccent,
+				KeyCode.Minus => ImGuiKey.Minus,
+				KeyCode.Plus => ImGuiKey.Equal,
+				KeyCode.LeftBracket => ImGuiKey.LeftBracket,
+				KeyCode.RightBracket => ImGuiKey.RightBracket,
+				KeyCode.Semicolon => ImGuiKey.Semicolon,
+				KeyCode.Quote => ImGuiKey.Apostrophe,
+				KeyCode.Comma => ImGuiKey.Comma,
+				KeyCode.Period => ImGuiKey.Period,
+				KeyCode.Slash => ImGuiKey.Slash,
+				KeyCode.Backslash => ImGuiKey.Backslash,
+				_ => ImGuiKey.None
+			};
+
+			return imguikey != ImGuiKey.None;
 		}
 	}
 }
